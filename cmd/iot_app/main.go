e7bd9f3f1bfc2cce96774529ed0f10bbc31222e2
Andrew Depke <andrewdepke@gmail.com>
2022-05-20 16:31:03 -0600

    Code cleanup pass

--- cmd/iot_app/main.go
+++ cmd/iot_app/main.go
@@ -1,6 +1,9 @@
+// This package implements a basic IoT app that features a local login system
+// for managing tokens, as well as simple querying and datapoint inserting.
+// Note that the login system is extraordinally simple, allowing for just a single
+// login at a time. This is for demonstration purposes.
+// It is a port of the Python sample found here: https://github.com/InfluxCommunity/iot_app
 package main
-
-// Port of https://github.com/InfluxCommunity/iot_app
 
 import (
 	"bytes"
@@ -32,25 +35,28 @@ type User struct {
 	writeToken string
 }
 
-var activeUser User
-var readClient influxdb2.Client
-var writeClient influxdb2.Client
-var url string = os.Getenv("INFLUXDB_HOST")
-var orgId string = os.Getenv("INFLUXDB_ORGANIZATION_ID")
-var bucket string = os.Getenv("INFLUX_BUCKET")
-var queryJson string
+var (
+	activeUser  User
+	readClient  influxdb2.Client
+	writeClient influxdb2.Client
+	queryJson   string
+
+	url    = os.Getenv("INFLUXDB_HOST")
+	orgId  = os.Getenv("INFLUXDB_ORGANIZATION_ID")
+	bucket = os.Getenv("INFLUX_BUCKET")
+)
 
 const loginDatabase = "logins.db"
 
-func createDefaultLoginDB() {
+// Connects to the local login database. Creates one with a default account if there isn't one.
+func getLoginDB() (*sql.DB, error) {
 	// If we don't have a login database yet, create one with a default user account.
 	if _, err := os.Stat(loginDatabase); errors.Is(err, os.ErrNotExist) {
 		fmt.Println("Failed to find logins database, creating a new one.")
 		db, err := sql.Open("sqlite3", loginDatabase)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
-		defer db.Close()
 
 		create := `CREATE TABLE user(
 			id INTEGER NOT NULL,
@@ -63,28 +69,32 @@ func createDefaultLoginDB() {
 			UNIQUE (email))`
 		_, err = db.Exec(create)
 		if err != nil {
-			fmt.Printf("Login table create failed: %q\n", err)
-			return
+			return db, fmt.Errorf("login table create failed: %q\n", err)
 		}
 
-		fmt.Println()
-		fmt.Println("Creating default user with login:\n\tEmail: mickey@mouse.com\n\tPassword: pass")
-		fmt.Println("Note that this account will not be able to access your influxdb organization.")
-		fmt.Println()
+		const defaultLoginMessage = `
+Creating default user with login:
+	Email: mickey@mouse.com
+	Password: pass
+Note that this account will not be able to access your influxdb organization.`
+
+		fmt.Println(defaultLoginMessage)
 
 		insert := `INSERT INTO user VALUES(
 			1,
 			'mickey@mouse.com',
 			'sha256$d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1',
 			'mickey',
-			'fVYoLl13o0ET6rhOEfpZoKcoOWofA9GE-Dv5P_EWI41fguTOoLuVD5HeGVEfgRVeF9xnYxh-sLcZEXGBkEFuWQ==',
-			'xOsoCvepHZzTLdr4WtWyoKNZ8KB-fzJ_4fvQpHIWS8GpBH7GqPP6dyock2cz1oVt5zar--N0AQ6frYBOYfevZg==')`
+			'my_read_token',
+			'my_write_token')`
 		_, err = db.Exec(insert)
 		if err != nil {
-			fmt.Printf("Login user insert failed: %q\n", err)
-			return
+			return db, fmt.Errorf("login user insert failed: %q\n", err)
 		}
 	}
+
+	// Database already exists, just open it.
+	return sql.Open("sqlite3", loginDatabase)
 }
 
 func tryLoginCredentials(user string, plainPassword string) bool {
@@ -94,23 +104,20 @@ func tryLoginCredentials(user string, plainPassword string) bool {
 	}
 	defer db.Close()
 
-	query := fmt.Sprintf(`SELECT * FROM user WHERE email='%s'`, user)
-	result, err := db.Query(query)
+	result, err := db.Query(`SELECT * FROM user WHERE email='$1'`, user)
 	if err != nil {
-		log.Fatal(err)
+		return false
 	}
 	defer result.Close()
 
 	for result.Next() {
-		var id int
-		var email string
-		var password string
-		var name string
-		var readToken string
-		var writeToken string
+		var (
+			id                                           int
+			email, password, name, readToken, writeToken string
+		)
 		err = result.Scan(&id, &email, &password, &name, &readToken, &writeToken)
 		if err != nil {
-			log.Fatal(err)
+			return false
 		}
 
 		hasher := sha256.New()
@@ -212,89 +219,96 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	t.Execute(w, data)
 }
 
-func setupWebHandlers() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		renderTemplate(w, "index", nil)
-	})
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", nil)
+}
 
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			w.WriteHeader(http.StatusOK)
-			renderTemplate(w, "login", nil)
-		case "POST":
-			email := r.FormValue("email")
-			password := r.FormValue("password")
-			fmt.Printf("Login post, retrieved credientials: email:%s, password:%s\n", email, password)
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		renderTemplate(w, "login", nil)
+	case "POST":
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		fmt.Printf("Login post, retrieved credientials: email:%s, password:%s\n", email, password)
 
-			// Query the login database to see if the credentials match.
-			if tryLoginCredentials(email, password) {
-				fmt.Println("Login success")
-				http.Redirect(w, r, "profile", http.StatusSeeOther)
-			} else {
-				fmt.Println("Login failed")
-				http.Error(w, "Invalid login", http.StatusForbidden)
-			}
+		// Query the login database to see if the credentials match.
+		if tryLoginCredentials(email, password) {
+			fmt.Println("Login success")
+			http.Redirect(w, r, "profile", http.StatusSeeOther)
+		} else {
+			fmt.Println("Login failed")
+			http.Error(w, "Invalid login", http.StatusForbidden)
 		}
-	})
+	}
+}
 
-	http.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
-		if !activeUser.valid {
-			fmt.Println("Not logged in, redirecting to login page.")
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	if !activeUser.valid {
+		fmt.Println("Not logged in, redirecting to login page.")
+		http.Redirect(w, r, "login", http.StatusSeeOther)
+	}
+
+	renderTemplate(w, "profile", map[string]interface{}{
+		"name":      activeUser.name,
+		"queryJson": queryJson,
+	})
+}
+
+func queryDataHandler(w http.ResponseWriter, r *http.Request) {
+	data := queryData(readClient)
+	queryJson = data
+	encoder := json.NewEncoder(w)
+	encoder.Encode(queryJson)
+}
+
+func writeDataHandler(w http.ResponseWriter, r *http.Request) {
+	writeData(writeClient)
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		renderTemplate(w, "signup", nil)
+	case "POST":
+		email := r.FormValue("email")
+		name := r.FormValue("name")
+		password := r.FormValue("password")
+		readToken := r.FormValue("readToken")
+		writeToken := r.FormValue("writeToken")
+
+		fmt.Printf("Registering new user: email=%s, name=%s, password=%s, readToken=%s, writeToken=%s\n",
+			email, name, password, readToken, writeToken)
+
+		if err := registerUser(email, name, password, readToken, writeToken); err != nil {
+			fmt.Println("Failed to register user:", err)
+			http.Error(w, "Failed to register user.", http.StatusBadRequest)
+		} else {
 			http.Redirect(w, r, "login", http.StatusSeeOther)
 		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
 
-		w.WriteHeader(http.StatusOK)
-		renderTemplate(w, "profile", map[string]interface{}{
-			"name":      activeUser.name,
-			"queryJson": queryJson,
-		})
-	})
-
-	http.HandleFunc("/graph_query_data", func(w http.ResponseWriter, r *http.Request) {
-		data := queryData(readClient)
-		queryJson = data
-		encoder := json.NewEncoder(w)
-		w.WriteHeader(http.StatusOK)
-		encoder.Encode(queryJson)
-	})
-
-	http.HandleFunc("/graph_write_data", func(w http.ResponseWriter, r *http.Request) {
-		writeData(writeClient)
-		w.WriteHeader(http.StatusOK)
-	})
-
-	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			w.WriteHeader(http.StatusOK)
-			renderTemplate(w, "signup", nil)
-		case "POST":
-			email := r.FormValue("email")
-			name := r.FormValue("name")
-			password := r.FormValue("password")
-			readToken := r.FormValue("readToken")
-			writeToken := r.FormValue("writeToken")
-
-			fmt.Printf("Registering new user: email=%s, name=%s, password=%s, readToken=%s, writeToken=%s\n",
-				email, name, password, readToken, writeToken)
-
-			if err := registerUser(email, name, password, readToken, writeToken); err != nil {
-				fmt.Println("Failed to register user:", err)
-				http.Error(w, "Failed to register user.", http.StatusBadRequest)
-			} else {
-				http.Redirect(w, r, "login", http.StatusSeeOther)
-			}
-		}
-	})
+func setupWebHandlers() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/profile", profileHandler)
+	http.HandleFunc("/graph_query_data", queryDataHandler)
+	http.HandleFunc("/graph_write_data", writeDataHandler)
+	http.HandleFunc("/signup", signupHandler)
 }
 
 func main() {
 	activeUser.valid = false
-	go createDefaultLoginDB()
+	db, err := getLoginDB()
+	if err != nil {
+		log.Fatalf("Get login db failed: %q", err)
+	}
+	defer db.Close()
 
-	// Url formatting.
+	// Ensure the influxdb url contains the https:// prefix. Add it if not.
 	httpsPrefix := "https://"
 	if !strings.Contains(url, httpsPrefix) {
 		url = strings.Join([]string{httpsPrefix, url}, "")
